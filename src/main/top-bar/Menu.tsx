c1c4c9c46903e9c975e419ccaa1b01ac5dbfc427
Marcus Balbi <balbimarcus@gmail.com>
2022-02-12 12:18:46 -0300

    add menu info

--- src/main/top-bar/Menu.tsx
+++ src/main/top-bar/Menu.tsx
@@ -6,50 +6,71 @@ import Divider from "@mui/material/Divider";
 import ListItem from "@mui/material/ListItem";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
-
+import { Typography } from "@mui/material";
+import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
+import PersonIcon from "@mui/icons-material/Person";
+import HealthAndSafetyIcon from "@mui/icons-material/HealthAndSafety";
+import PlaceIcon from "@mui/icons-material/Place";
+import SettingsIcon from "@mui/icons-material/Settings";
+import ComputerIcon from "@mui/icons-material/Computer";
 interface Props {
   opened: boolean;
-  onClose?: () => void
+  onClose?: () => void;
 }
 
 export default function Menu(props: Props) {
   const list = () => (
-    <Box
-      sx={{ width: 250 }}
-      role="presentation"
-    >
+    <Box sx={{ width: 250 }} role="presentation">
+      <Typography align="center" variant="h5" mt={2} mb={2}>
+        LOGO AQUI
+      </Typography>
+      <Divider />
       <List>
-        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
+        <ListItem button key={"cadastrar_paciente"}>
+          <ListItemIcon>
+            <PersonIcon />
+          </ListItemIcon>
+          <ListItemText primary="Paciente" />
+        </ListItem>
+        <ListItem button key={"cadastrar_atendente"}>
+          <ListItemIcon>
+            <ComputerIcon />
+          </ListItemIcon>
+          <ListItemText primary="Atendente" />
+        </ListItem>
+        <ListItem button key={"cadastrar_medico"}>
+          <ListItemIcon>
+            <HealthAndSafetyIcon />
+          </ListItemIcon>
+          <ListItemText primary="Medico" />
+        </ListItem>
+        <ListItem button key={"local_atendimento"}>
+          <ListItemIcon>
+            <PlaceIcon />
+          </ListItemIcon>
+          <ListItemText primary="Local de Atendimento" />
+        </ListItem>
       </List>
       <Divider />
       <List>
-        {["All mail", "Trash", "Spam"].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
+        <ListItem button key={"agenda"}>
+          <ListItemIcon>
+            <CalendarTodayIcon />
+          </ListItemIcon>
+          <ListItemText primary="Agenda" />
+        </ListItem>
+        <ListItem button key={"config"}>
+          <ListItemIcon>
+            <SettingsIcon />
+          </ListItemIcon>
+          <ListItemText primary="Configurações" />
+        </ListItem>
       </List>
     </Box>
   );
 
   return (
-    <Drawer
-      anchor={"left"}
-      open={props.opened}
-      onClose={props.onClose}
-    >
+    <Drawer anchor={"left"} open={props.opened} onClose={props.onClose}>
       {list()}
     </Drawer>
   );
