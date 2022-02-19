e795c0908d006c89a9590f34ba081b852cb6f4c7
Marcus Balbi <balbimarcus@gmail.com>
2022-02-19 08:22:51 -0300

    transform jsx structure into data structure

--- src/main/top-bar/Menu.tsx
+++ src/main/top-bar/Menu.tsx
@@ -13,12 +13,62 @@ import HealthAndSafetyIcon from "@mui/icons-material/HealthAndSafety";
 import PlaceIcon from "@mui/icons-material/Place";
 import SettingsIcon from "@mui/icons-material/Settings";
 import ComputerIcon from "@mui/icons-material/Computer";
+import { NavigateOptions, To, useNavigate } from "react-router-dom";
 interface Props {
   opened: boolean;
   onClose?: () => void;
 }
 
+const menuItemsCrud = [
+  {
+    key: "paciente",
+    route: "/paciente",
+    icon: <PersonIcon />,
+    text: "Paciente",
+  },
+  {
+    key: "atendente",
+    route: "/atendente",
+    icon: <ComputerIcon />,
+    text: "Atendente",
+  },
+  {
+    key: "medico",
+    route: "/medico",
+    icon: <HealthAndSafetyIcon />,
+    text: "Médico",
+  },
+  {
+    key: "local_atendimento",
+    route: "/local-atendimento",
+    icon: <PlaceIcon />,
+    text: "Local de Atendimento",
+  },
+];
+
+const menuItemsOperations = [
+  {
+    key: "agenda",
+    route: "/agenda",
+    icon: <CalendarTodayIcon />,
+    text: "Agenda",
+  },
+  {
+    key: "config",
+    route: "/config",
+    icon: <SettingsIcon />,
+    text: "Configurações",
+  },
+];
+
 export default function Menu(props: Props) {
+  const navigate = useNavigate();
+
+  const menuNavigate = (to: To, options?: NavigateOptions | undefined) => {
+    navigate(to, options);
+    props.onClose && props.onClose();
+  };
+
   const list = () => (
     <Box sx={{ width: 250 }} role="presentation">
       <Typography align="center" variant="h5" mt={2} mb={2}>
@@ -26,45 +76,33 @@ export default function Menu(props: Props) {
       </Typography>
       <Divider />
       <List>
-        <ListItem button key={"cadastrar_paciente"}>
-          <ListItemIcon>
-            <PersonIcon />
-          </ListItemIcon>
-          <ListItemText primary="Paciente" />
-        </ListItem>
-        <ListItem button key={"cadastrar_atendente"}>
-          <ListItemIcon>
-            <ComputerIcon />
-          </ListItemIcon>
-          <ListItemText primary="Atendente" />
-        </ListItem>
-        <ListItem button key={"cadastrar_medico"}>
-          <ListItemIcon>
-            <HealthAndSafetyIcon />
-          </ListItemIcon>
-          <ListItemText primary="Medico" />
-        </ListItem>
-        <ListItem button key={"local_atendimento"}>
-          <ListItemIcon>
-            <PlaceIcon />
-          </ListItemIcon>
-          <ListItemText primary="Local de Atendimento" />
-        </ListItem>
+        {menuItemsCrud.map((info) => {
+          return (
+            <ListItem
+              button
+              key={info.key}
+              onClick={() => menuNavigate(info.route)}
+            >
+              <ListItemIcon>{info.icon}</ListItemIcon>
+              <ListItemText primary={info.text} />
+            </ListItem>
+          );
+        })}
       </List>
       <Divider />
       <List>
-        <ListItem button key={"agenda"}>
-          <ListItemIcon>
-            <CalendarTodayIcon />
-          </ListItemIcon>
-          <ListItemText primary="Agenda" />
-        </ListItem>
-        <ListItem button key={"config"}>
-          <ListItemIcon>
-            <SettingsIcon />
-          </ListItemIcon>
-          <ListItemText primary="Configurações" />
-        </ListItem>
+        {menuItemsOperations.map((info) => {
+          return (
+            <ListItem
+              button
+              key={info.key}
+              onClick={() => menuNavigate(info.route)}
+            >
+              <ListItemIcon>{info.icon}</ListItemIcon>
+              <ListItemText primary={info.text} />
+            </ListItem>
+          );
+        })}
       </List>
     </Box>
   );
