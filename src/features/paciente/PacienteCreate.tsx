9d78fd3f1ac8cfbaeb21433f4566f108398803a0
Marcus Balbi <balbimarcus@gmail.com>
2022-03-06 12:03:16 -0300

    remove Page from baseForm

--- src/features/paciente/PacienteCreate.tsx
+++ src/features/paciente/PacienteCreate.tsx
@@ -1,6 +1,6 @@
 import { Grid, TextField } from "@mui/material";
 import * as React from "react";
-import { UFSelect, BaseForm, FormInfoSection } from "../../shared";
+import { UFSelect, BaseForm, FormInfoSection, Page } from "../../shared";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 
@@ -20,102 +20,108 @@ const PacienteCreate = () => {
   const onSubmit: SubmitHandler<CadastrarPacienteDto> = (data) =>
     console.log(data);
   return (
-    <BaseForm isDirty={isDirty} onSubmit={handleSubmit(onSubmit)}>
-      <FormInfoSection>Informações Pessoais</FormInfoSection>
-      <Grid item xs={12}>
-        <TextField
-          fullWidth
-          label="Nome Completo"
-          placeholder="Ex: João Silva"
-          error={!!errors.nome?.message}
-          helperText={errors.nome?.message}
-          {...register("nome")}
-        />
-      </Grid>
-      <Grid item xs={12} md={6}>
-        <TextField
-          fullWidth
-          label="CPF"
-          placeholder="00000000000"
-          error={!!errors.cpf?.message}
-          helperText={errors.cpf?.message}
-          {...register("cpf")}
-        />
-      </Grid>
-      <Grid item xs={12} md={6}>
-        <TextField
-          fullWidth
-          label="Date de Nascimento"
-          placeholder="00/00/0000"
-          error={!!errors.dataNasciemnto?.message}
-          helperText={errors.dataNasciemnto?.message}
-          {...register("dataNasciemnto")}
-        />
-      </Grid>
-      <FormInfoSection>Endereço</FormInfoSection>
-      <Grid item xs={12} md={10}>
-        <TextField fullWidth label="Logradouro" {...register("logradouro")} />
-      </Grid>
-      <Grid item xs={12} md={2}>
-        <TextField fullWidth label="Número" {...register("numero")} />
-      </Grid>
-      <Grid item xs={12} md={2}>
-        <TextField
-          fullWidth
-          label="CEP"
-          error={!!errors.cep?.message}
-          helperText={errors.cep?.message}
-          {...register("cep")}
-        />
-      </Grid>
-      <Grid item xs={12} md={4}>
-        <TextField fullWidth label="Bairro" {...register("bairro")} />
-      </Grid>
-      <Grid item xs={12} md={4}>
-        <TextField fullWidth label="Cidade" {...register("cidade")} />
-      </Grid>
-      <Grid item xs={12} md={2}>
-        <UFSelect
-          label="UF"
-          fullWidth
-          {...register("estado")}
-          onChange={(e) => {
-            setValue("estado", e.target.value as string);
-          }}
-        />
-      </Grid>
-      <Grid item xs={12}>
-        <TextField fullWidth label="Complemento" {...register("complemento")} />
-      </Grid>
-      <FormInfoSection>Contato</FormInfoSection>
-      <Grid item xs={12} md={4}>
-        <TextField
-          fullWidth
-          label="Telefone Celular"
-          error={!!errors.telefoneCelular?.message}
-          helperText={errors.telefoneCelular?.message}
-          {...register("telefoneCelular")}
-        />
-      </Grid>
-      <Grid item xs={12} md={4}>
-        <TextField
-          fullWidth
-          label="Email"
-          error={!!errors.email?.message}
-          helperText={errors.email?.message}
-          {...register("email")}
-        />
-      </Grid>
-      <Grid item xs={12} md={4}>
-        <TextField
-          fullWidth
-          error={!!errors.telefoneContato?.message}
-          helperText={errors.telefoneContato?.message}
-          label="Telefone para Contato"
-          {...register("telefoneContato")}
-        />
-      </Grid>
-    </BaseForm>
+    <Page>
+      <BaseForm isDirty={isDirty} onSubmit={handleSubmit(onSubmit)}>
+        <FormInfoSection>Informações Pessoais</FormInfoSection>
+        <Grid item xs={12}>
+          <TextField
+            fullWidth
+            label="Nome Completo"
+            placeholder="Ex: João Silva"
+            error={!!errors.nome?.message}
+            helperText={errors.nome?.message}
+            {...register("nome")}
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <TextField
+            fullWidth
+            label="CPF"
+            placeholder="00000000000"
+            error={!!errors.cpf?.message}
+            helperText={errors.cpf?.message}
+            {...register("cpf")}
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <TextField
+            fullWidth
+            label="Date de Nascimento"
+            placeholder="00/00/0000"
+            error={!!errors.dataNasciemnto?.message}
+            helperText={errors.dataNasciemnto?.message}
+            {...register("dataNasciemnto")}
+          />
+        </Grid>
+        <FormInfoSection>Endereço</FormInfoSection>
+        <Grid item xs={12} md={10}>
+          <TextField fullWidth label="Logradouro" {...register("logradouro")} />
+        </Grid>
+        <Grid item xs={12} md={2}>
+          <TextField fullWidth label="Número" {...register("numero")} />
+        </Grid>
+        <Grid item xs={12} md={2}>
+          <TextField
+            fullWidth
+            label="CEP"
+            error={!!errors.cep?.message}
+            helperText={errors.cep?.message}
+            {...register("cep")}
+          />
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <TextField fullWidth label="Bairro" {...register("bairro")} />
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <TextField fullWidth label="Cidade" {...register("cidade")} />
+        </Grid>
+        <Grid item xs={12} md={2}>
+          <UFSelect
+            label="UF"
+            fullWidth
+            {...register("estado")}
+            onChange={(e) => {
+              setValue("estado", e.target.value as string);
+            }}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            fullWidth
+            label="Complemento"
+            {...register("complemento")}
+          />
+        </Grid>
+        <FormInfoSection>Contato</FormInfoSection>
+        <Grid item xs={12} md={4}>
+          <TextField
+            fullWidth
+            label="Telefone Celular"
+            error={!!errors.telefoneCelular?.message}
+            helperText={errors.telefoneCelular?.message}
+            {...register("telefoneCelular")}
+          />
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <TextField
+            fullWidth
+            label="Email"
+            error={!!errors.email?.message}
+            helperText={errors.email?.message}
+            {...register("email")}
+          />
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <TextField
+            fullWidth
+            error={!!errors.telefoneContato?.message}
+            helperText={errors.telefoneContato?.message}
+            label="Telefone para Contato"
+            {...register("telefoneContato")}
+          />
+        </Grid>
+      </BaseForm>
+    </Page>
   );
 };
 
--- src/shared/Form.tsx
+++ src/shared/Form.tsx
@@ -4,7 +4,7 @@ import * as React from "react";
 import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 import { useTheme, Typography } from "@material-ui/core";
-import { ActionBar, Page } from ".";
+import { ActionBar } from ".";
 
 interface CreateFormProps {
   children?: any;
@@ -27,45 +27,43 @@ const BaseForm = (props: CreateFormProps) => {
   const navigate = useNavigate();
   const theme = useTheme();
   return (
-    <Page>
-      <form onSubmit={props.onSubmit}>
-        <Grid container spacing={2}>
-          {props.children}
-          <Grid item xs={12}>
-            <ActionBar>
-              <Button type="submit" color="success">
-                Cadastrar
-              </Button>
-              <Button
-                onClick={() => {
-                  if (props.isDirty) {
-                    Swal.fire({
-                      title: "Tem certeza?",
-                      text: "você pode perder informações!",
-                      icon: "warning",
-                      showCancelButton: true,
-                      confirmButtonColor: theme.palette.error.dark,
-                      cancelButtonColor: theme.palette.primary.main,
-                      confirmButtonText: "Sair",
-                      cancelButtonText: "Continuar",
-                    }).then((result) => {
-                      if (result.isConfirmed) {
-                        navigate(-1);
-                      }
-                    });
-                  } else {
-                    navigate(-1);
-                  }
-                }}
-                color="error"
-              >
-                Voltar
-              </Button>
-            </ActionBar>
-          </Grid>
+    <form onSubmit={props.onSubmit}>
+      <Grid container spacing={2}>
+        {props.children}
+        <Grid item xs={12}>
+          <ActionBar>
+            <Button type="submit" color="success">
+              Cadastrar
+            </Button>
+            <Button
+              onClick={() => {
+                if (props.isDirty) {
+                  Swal.fire({
+                    title: "Tem certeza?",
+                    text: "você pode perder informações!",
+                    icon: "warning",
+                    showCancelButton: true,
+                    confirmButtonColor: theme.palette.error.dark,
+                    cancelButtonColor: theme.palette.primary.main,
+                    confirmButtonText: "Sair",
+                    cancelButtonText: "Continuar",
+                  }).then((result) => {
+                    if (result.isConfirmed) {
+                      navigate(-1);
+                    }
+                  });
+                } else {
+                  navigate(-1);
+                }
+              }}
+              color="error"
+            >
+              Voltar
+            </Button>
+          </ActionBar>
         </Grid>
-      </form>
-    </Page>
+      </Grid>
+    </form>
   );
 };
 
