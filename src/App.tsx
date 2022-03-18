6fe8f405d871b34f35f7dbeb58181d8ca839205c
Marcus Balbi <balbimarcus@gmail.com>
2022-03-17 21:29:15 -0300

    crate auth context

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,20 @@
 import React, { useEffect } from "react";
+import { useAuth } from "./hooks/auth";
 import AppRoutes from "./main/routes/Routes";
 import { TopBar } from "./main/top-bar";
-import { request } from "./shared";
 
 function App() {
+  const { signIn, dados } = useAuth();
   useEffect(() => {
-    const login = localStorage.getItem("login");
-    if (!login) {
-      request
-        .post("/auth/signin", {
-          username: process.env.REACT_APP_SAMPLE_USERNAME,
-          password: process.env.REACT_APP_SAMPLE_PASSWORD,
-        })
-        .then(({ data }) => {
-          localStorage.setItem("login", JSON.stringify(data));
-        });
+    if (!dados?.token) {
+      signIn({
+        username: process.env.REACT_APP_SAMPLE_USERNAME || "",
+        password: process.env.REACT_APP_SAMPLE_PASSWORD || "",
+      });
     }
-  }, []);
+  }, [dados, signIn]);
   return (
     <React.Fragment>
-      <div>
-        {JSON.stringify({
-          username: process.env.REACT_APP_SAMPLE_USERNAME,
-          password: process.env.REACT_APP_SAMPLE_PASSWORD,
-          host: process.env.REACT_APP_BACKEND_URL,
-        })}
-      </div>
       <TopBar />
       <>
         <AppRoutes />
