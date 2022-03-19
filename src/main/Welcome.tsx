a0f4e39c2ad21867e7d3866bcca949eacb085a5f
Marcus Balbi <balbimarcus@gmail.com>
2022-03-19 11:56:39 -0300

    fix welcome

--- src/main/Welcome.tsx
+++ src/main/Welcome.tsx
@@ -1,13 +1,13 @@
-import { Typography } from "@mui/material";
+import { CircularProgress, Typography } from "@mui/material";
 import { useAuth } from "../hooks/auth";
 import { Page } from "../shared";
 
 const Welcome = () => {
   const { dados } = useAuth();
-  console.log(dados);
   return (
     <Page>
-      <Typography variant="h3">Bem vindo, {dados.usuario} !</Typography>
+      {dados ?
+        <Typography variant="h3">Bem vindo, {dados.usuario} !</Typography> : <CircularProgress />}
     </Page>
   );
 };
