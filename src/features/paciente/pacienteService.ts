303218ee587c3b2bcdb92ee3b124a0b824d6462c
Marcus Balbi <balbimarcus@gmail.com>
2022-03-22 20:54:45 -0300

    convert data and request logger

--- src/features/paciente/pacienteService.ts
+++ src/features/paciente/pacienteService.ts
@@ -1,12 +1,19 @@
 import { request } from "../../shared";
 import { CadastrarPacienteDto } from "./pacienteDto";
+import { cloneDeep } from "lodash";
+import { parse } from "date-fns";
 
-const createPaciente = (paciente: CadastrarPacienteDto) => {
+const createPaciente = async (paciente: CadastrarPacienteDto) => {
   // prepare data to be sent:
-  const parsedData  = paciente;
-  request.post("/pacientes", parsedData);
+  const parsedData = cloneDeep(paciente);
+  parsedData.dataNascimento = parse(
+    parsedData.dataNascimento,
+    "dd/MM/yyyy",
+    new Date()
+  ).toISOString();
+  const result = await request.post("/pacientes", parsedData);
 
-  console.log(paciente, "============================================");
+  console.log(result, "============================================");
 };
 
 const listPaciente = async () => {
--- src/shared/request.ts
+++ src/shared/request.ts
@@ -4,4 +4,25 @@ const request = axios.create({
   baseURL: process.env.REACT_APP_BACKEND_URL,
 });
 
+request.interceptors.request.use((config) => {
+  if (process.env.NODE_ENV !== "production") {
+    console.log("REQUEST_LOGGER", {
+      method: config.method,
+      url: config.url,
+      data: config.data,
+    });
+  }
+  return config;
+});
+
+request.interceptors.response.use((config) => {
+  if (process.env.NODE_ENV !== "production") {
+    console.log("RESPONSE_LOGGER", {
+      data: config.data,
+      status: config.status,
+    });
+  }
+  return config;
+});
+
 export { request };
