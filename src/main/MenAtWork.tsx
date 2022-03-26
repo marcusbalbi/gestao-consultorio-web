dfb6c46a63ae81900604975ae97c148e3c4b1c3f
Marcus Balbi <balbimarcus@gmail.com>
2022-03-25 23:34:01 -0300

    ramdom menatwork

--- src/main/MenAtWork.tsx
+++ src/main/MenAtWork.tsx
@@ -1,4 +1,4 @@
-import { Handyman } from "@mui/icons-material";
+import { Handyman, Carpenter, SquareFoot } from "@mui/icons-material";
 import { Button } from "@mui/material";
 import { Box } from "@mui/system";
 import { useNavigate } from "react-router-dom";
@@ -6,6 +6,17 @@ import { Page } from "../shared";
 
 const MenAtWork = () => {
   const navigate = useNavigate();
+  const renderConstructionIcon = () => {
+    const components = [
+      <Handyman style={{ fontSize: "12rem" }} />,
+      <SquareFoot style={{ fontSize: "12rem" }} />,
+      <Carpenter style={{ fontSize: "12rem" }} />,
+    ];
+
+    const ramdom = Math.floor(Math.random() * 2);
+
+    return components[ramdom];
+  };
   return (
     <Page title="Em Breve">
       <Box
@@ -14,7 +25,7 @@ const MenAtWork = () => {
         alignItems="center"
         flexDirection={"column"}
       >
-        <Handyman style={{ fontSize: "12rem" }} />
+        {renderConstructionIcon()}
         <Button
           onClick={() => {
             navigate(-1);
